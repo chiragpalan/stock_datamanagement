1905a286f262ee43310d7dd1f14062d145177d65
chirag <61842354+chiragpalan@users.noreply.github.com>
2024-12-30 16:21:32 +0530

    Update create_and_save_temp_csv.py

--- create_and_save_temp_csv.py
+++ create_and_save_temp_csv.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import os
 
 # Create the directory if it doesn't exist
-os.makedirs("temp_csvs", exist_ok=True)
+# os.makedirs("temp_csvs", exist_ok=True)
 
 # List of tickers
 tickers = ["RELIANCE.NS", "TCS.NS", "INFY.NS", "HDFCBANK.NS", "ICICIBANK.NS"]
@@ -16,7 +16,7 @@ for ticker in tickers:
     if not stock_data.empty:
         # Format the ticker for the file name
         file_name = ticker.replace(".", "_")
-        
+        print(stock_data)
         # Save the data to a CSV file in the temp_csvs folder
         stock_data.to_csv(f"temp_csvs/{file_name}.csv")
         print(f"Data for {ticker} saved to temp_csvs/{file_name}.csv")
